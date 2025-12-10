da4654cfc08b8161420219e0e14a723a389cec69
AEscamilla9804 <alanmauricio.escamilla@gmail.com>
2025-12-10 09:10:04 -0600

    Update db.ts for deployment

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,14 @@ dotenv.config()
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
     dialect: "postgres",
+    protocol: "postgres",
     models: [__dirname + '/../models/**/*'],
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     logging: false
 });
 
